c3a2209ce5e956a5462d773b217541587562e24c
David Rodriguez <davidrodriguezcomedy@gmail.com>
2022-05-28 15:13:29 -0500

    initial add of the files

---
+++ script.js
@@ -0,0 +1,62 @@
+window.alert("Would you like to create a password");
+console.log([0])
+
+
+// create variables to hold chars option (special, number Uppercase, Lowercase)
+
+
+//   var specialChars = ["!","@","#","$","%","^","&","*"];
+//   var NumberChars = Array["1","2","3","4","5","6","7","8","9"];
+//   var Uppercase = true;
+//   var Lowercase = true;
+
+//   var allOptions = array(specialChars, NumberChars, Uppercase, Lowercase);
+
+// // create an empty array to hold users selectedchars
+// var userSelection = []
+
+// //on game start prompt users number of characters and hold in a variable "userInput"
+
+// var howMany = prompt("How Many Characters Would like in your Password"); 
+
+
+// //check the users input and check that they put in a number between 8 & 128
+
+
+
+
+// // write the function that validates users input 
+
+// function validateUserInput () {
+// //need to create user input function
+
+// }
+
+
+
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // // Write password to the #password input
+// // function writePassword(allOptions) {
+// //   var password = generatePassword() {
+// //     return: Math,floor(Math.random() * max);
+// //   }
+// // console.log(writepassword[8])
+
+
+// //   var passwordText = document.querySelector("#password");
+
+// //   passwordText.value = password;
+
+// // }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+// function getRandomInt(specialChars) {
+//   return Math.floor(Math.random() * max);
+// }
+// console.log(getRandomInt);
